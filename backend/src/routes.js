e2fd12a8bb2303bc1e668fe72541b11ed2eda3c7
Vincially <viniciuscosta.developer@gmail.com>
2020-03-28 03:21:17 -0300

    Logon and Register integrated

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
-routes.post('/session', SessionController.create);
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import {FiLogIn} from 'react-icons/fi/';
+import api from '../../services/api';
 
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+    async function handleLogin(event) {
+        event.preventDefault();
+        try {
+            const response = await api.post('sessions',{id});
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile')
+        }
+        catch(erro) {
+            alert('Falha no login, tente novamente.')
+        }
+    }
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} alt="Be The Hero"/>
-                <form action="">
+                <img src={logoImg} alt  ="Be The Hero"/>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID"/>
+                    <input placeholder="Sua ID" value={id} onChange={event => setId(event.target.value)}/>
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
