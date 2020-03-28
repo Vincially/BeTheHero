198361063944c71b74789cf0299d26d706d71178
Vincially <viniciuscosta.developer@gmail.com>
2020-03-28 01:18:58 -0300

    Front-End finished with routes

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import {FiLogIn} from "react-icons/fi/"
-
+import React from 'react';
 import './styles.css';
-import logoImg from "../../assets/logo.svg";
-import heroesImg from "../../assets/heroes.png";
+import {FiLogIn} from 'react-icons/fi/';
+
+import {Link} from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
-
                 <form action="">
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não Tenho Cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
